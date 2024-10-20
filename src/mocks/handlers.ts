728bfe2fa2079c0ce1239e1e8b26851770bbe114
mr_noppo <takizawa20383461@gmail.com>
2024-10-20 18:01:23 +0900

    msw button

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@
 import { http } from 'msw'
  
 export const handlers = [
-  http.get('https://qiita.com/', () => {
+  http.get('https://jsonplaceholder.typicode.com/todos/1', () => {
     console.log('Captured a "GET /posts" request')
   }),
   http.post('/posts', () => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,13 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+    const access_db = async () => {
+
+        await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        return;
+    };
   return (
+    
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
@@ -53,6 +59,9 @@ export default function Home() {
             />
             Deploy now
           </a>
+          <button onClick={access_db} className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+            Access DB
+          </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
